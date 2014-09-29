a0f46b6244b81136246b46b184954ee3b7cc74e6
Mina Khan <minakhan01@gmail.com>
2014-09-29 00:01:24 -0400

    add files

---
+++ HW3/Spreadsheet_MVC/src/Cell.java
@@ -0,0 +1,17 @@
+
+public class Cell {
+	int value;
+	
+	public Cell(){
+		value=0;
+	}
+	
+	public void setValue(int value){
+		this.value=value;
+	}
+	
+	public int getValue(){
+		return value;
+	}
+	
+}
